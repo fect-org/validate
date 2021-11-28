6c563f19256bbf5bbe83018a761a0795da0b7416
kanno <812137533@qq.com>
2021-11-28 14:55:41 +0800

    feat: add basic verify rule

---
+++ lib/verify/basic-type.d.ts
@@ -0,0 +1,9 @@
+export declare const array: (val: any) => boolean;
+
+export declare const number: (val: any) => boolean;
+
+export declare const object: (val: any) => boolean;
+
+export declare const string: (val: any) => boolean;
+
+export declare const boolean: (val: any) => boolean;
